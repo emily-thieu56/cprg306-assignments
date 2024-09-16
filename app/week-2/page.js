6faa825d0c90180540efde3fdf0b847c937ad63c
emily-thieu56 <152931248+emily-thieu56@users.noreply.github.com>
2024-09-16 12:24:40 -0600

    Added content to student info
    Installed packages and added content to student info.

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,11 @@
+import Student_Component from "./student-info";
+import Link from "next/link";
+
 export default function Page() {
     return (
       <main>
         <h1>Shopping List</h1>
+        <Student_Component></Student_Component>
       </main>
     );
   }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Student_Component() {
+    return (
+      <main>
+        <p>Emily Thieu</p>
+        <Link href="https://github.com/"> </Link>
+      </main>
+    );
+  }
