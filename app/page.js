942179ca964085dcb67c074608fbe3331bda99db
emily-thieu56 <152931248+emily-thieu56@users.noreply.github.com>
2024-09-16 12:47:43 -0600

    Added content to root page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,12 @@
-
+import { Main } from "next/document"
+import Link from "next/link";
 
 export default function Home() {
- 
+ return (
+  <main>
+    <h1>CPRG 306: Web Development 2 - Assignments</h1>
+    <Link href="http://localhost:3000/week-2"> Week 2 Assignment</Link>
+  </main>
+  
+ );
 }
